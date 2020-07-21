bd8191c7c57e07e548c69494ecd84c3ec748eee8
eoleahy <eoin.97@live.ie>
2020-07-21 21:45:27 +0100

    adding new command

---
+++ app/commands/coom.js
@@ -0,0 +1,23 @@
+const opusscript = require('opusscript');
+const fs = require('fs');
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'coom',
+    description: 'Plays audio',
+    async execute(msg){
+        const audio_path = './app/src/audio/coomer.ogg';
+        let vc = msg.member.voice.channel;
+        if(!vc)
+            return msg.reply('Join a voice channel there mate.');
+
+        const connection = await vc.join();
+
+        const dispatcher = connection.play(fs.createReadStream(audio_path), {type: 'ogg/opus', volume: .25});
+
+        dispatcher.on('finish', () => {
+            console.log('Audio is finished');
+            vc.leave();
+        });
+    }
+}
